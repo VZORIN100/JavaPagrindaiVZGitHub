002a91ddea9ec5788bcfcc0eb28bfb5d09602363
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-09 18:02:28 +0400

    4.1uzd.04_04.NumberOfNumbers incomplete.

--- 4/4.1 uzduotis/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ 4/4.1 uzduotis/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -1,24 +1,24 @@
-
-import java.util.Scanner;
-
-public class NumberOfNumbers {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Give a number:");
-            int number = Integer.parseInt(scanner.nextLine());
-            if (number > 0) {
-                sum++;
-            } else if (number == 0) {
-                break;
-            } else {
-                System.out.println(number * number);
-            }
-        }
-    }
-}
+//
+//import java.util.Scanner;
+//
+//public class NumberOfNumbers {
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (true) {
+//            System.out.println("Give a number:");
+//            int number = Integer.parseInt(scanner.nextLine());
+//            if (number > 0) {
+//                sum++;
+//            } else if (number == 0) {
+//                break;
+//            } else {
+//                System.out.println(number * number);
+//            }
+//        }
+//    }
+//}
 
 
 
