80e3f9744504769a057e6be69b21ebb3edd45bcd
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-09 17:40:20 +0400

    end of day commit.

--- 4/4.1 uzduotis/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ 4/4.1 uzduotis/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -1,18 +1,24 @@
 
 import java.util.Scanner;
 
-public class AreWeThereYet {
+public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        while (true) {
-            System.out.println("Give a number");
-            int number = Integer.parseInt(scanner.nextLine());
 
-            if (number == 4) {
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number > 0) {
+                sum++;
+            } else if (number == 0) {
                 break;
+            } else {
+                System.out.println(number * number);
             }
         }
     }
 }
+
+
+
