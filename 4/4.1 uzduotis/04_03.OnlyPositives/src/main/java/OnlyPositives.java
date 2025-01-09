25b9ba36cebb961e13ef948d3cf3c99e9416ec3c
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-09 17:57:57 +0400

    4.1uzd.04_03.OnlyPositives task completed again.

--- 4/4.1 uzduotis/04_03.OnlyPositives/src/main/java/OnlyPositives.java
+++ 4/4.1 uzduotis/04_03.OnlyPositives/src/main/java/OnlyPositives.java
@@ -1,22 +1,20 @@
-//
-//import java.util.Scanner;
-//
-//public class OnlyPositives {
-//
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        int sum = 0;
-//
-//        while (true) {
-//            System.out.println("Give a number:");
-//            int number = Integer.parseInt(scanner.nextLine());
-//            if (number != 0) {
-//                sum++;
-//            } else {
-//                System.out.println("Number of numbers: " + sum);
-//                break;
-//            }
-//        }
-//    }
-//}
+import java.util.Scanner;
+
+public class OnlyPositives {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+            } else if (number == 0) {
+                break;
+            } else {
+                System.out.println(number * number);
+            }
+        }
+    }
+}
