23b64282c1ee2ac86db98cef0029735f319f0a12
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-10 16:00:55 +0400

    05_03 incomplete

--- 5/5.1 uzduotis/05_03.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ 5/5.1 uzduotis/05_03.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,34 @@
-
+import java.util.Scanner;
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
 
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            printUntilNumber();
+        }
     }
 
+    public static void printUntilNumber() {
+        // Write some code in here
+        System.out.println("Number");
+
+    }
 }
+
+//public class FromOneToParameter {
+//
+//    public static void main(String[] args) {
+//        int x = 1;
+//        System.out.println("Before the call, x is " + x);
+//        printUntilNumber(x);
+//        System.out.println("After the call, x is " + x);
+//    }
+//
+//    public static void printUntilNumber(int x) {
+//        // Write some code in here
+//        x++;
+//        System.out.println("x inside the method is " + x);
+//    }
+//}
