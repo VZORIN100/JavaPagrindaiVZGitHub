24487b41287c719f209063d6b0304a0d8a4194d8
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-21 18:57:03 +0400

    7.ArrayList_prak.AL_13.AverageOfAList task complete.

--- 7/ArrayList_praktiniai/AL_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ 7/ArrayList_praktiniai/AL_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -11,6 +11,26 @@ public class AverageOfAList {
         // adding them on a list until user gives -1.
         // Then it computes the average of the numbers on the list
         // and prints it.
-        
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            sum = sum + number;
+        }
+        double average = (1.0 * sum) / list.size();
+        System.out.println("Average: " + average);
+
     }
 }
