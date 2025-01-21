5e07d13c0d7aa3ae90b73c7dcc415a10fd089752
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-21 17:46:30 +0400

    6.1uzd.06_05.PrintInStars task compete. Tests passed.

--- 6/6.1 uzduotis/06_05.PrintInStars/src/main/java/Printer.java
+++ 6/6.1 uzduotis/06_05.PrintInStars/src/main/java/Printer.java
@@ -9,6 +9,14 @@ public class Printer {
 
     public static void printArrayInStars(int[] array) {
         // Write some code in here
+        int stars = 0;
+        for (int i = 0; i < array.length; i++) {
+            stars = array[i];
+            for (int s = 0; s < stars; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
 }
