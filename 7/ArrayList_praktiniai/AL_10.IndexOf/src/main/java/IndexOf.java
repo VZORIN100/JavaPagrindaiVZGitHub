45cead084f822eb3243c40a439f502605b261a53
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-21 18:40:13 +0400

    7.ArrayList_prak.AL_10.IndexOf task complete.

--- 7/ArrayList_praktiniai/AL_10.IndexOf/src/main/java/IndexOf.java
+++ 7/ArrayList_praktiniai/AL_10.IndexOf/src/main/java/IndexOf.java
@@ -20,5 +20,15 @@ public class IndexOf {
         System.out.println("");
 
         // implement here finding the indices of a number
+        System.out.println("Search for? ");
+        int numberWhich = Integer.parseInt(scanner.nextLine());
+        System.out.println("");
+
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if (number == numberWhich) {
+                System.out.println("" + numberWhich + " is at index " + i);
+            }
+        }
     }
 }
