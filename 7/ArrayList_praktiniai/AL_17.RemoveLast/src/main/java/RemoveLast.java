7be35c4bc2aede1efbb8748b571c0bf5fc96bdd4
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-21 19:37:39 +0400

    7.ArrayList_prak.AL_17.RemoveLast task complete.

--- 7/ArrayList_praktiniai/AL_17.RemoveLast/src/main/java/RemoveLast.java
+++ 7/ArrayList_praktiniai/AL_17.RemoveLast/src/main/java/RemoveLast.java
@@ -5,6 +5,20 @@ public class RemoveLast {
 
     public static void main(String[] args) {
         // Try your method in here
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+        removeLast(strings);
+        removeLast(strings);
+        System.out.println(strings);
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        int size = strings.size();
+        if (size != 0) strings.remove(size - 1);
     }
 
 }
