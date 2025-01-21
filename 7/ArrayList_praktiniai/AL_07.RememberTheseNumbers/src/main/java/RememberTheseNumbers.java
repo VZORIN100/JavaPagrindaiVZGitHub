6bc0ccf626d1929f4254246a3b4029b1f7e21492
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-21 18:13:40 +0400

    7.ArrayList_prak.AL_07.RememberTheseNumbers task complete.

--- 7/ArrayList_praktiniai/AL_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
+++ 7/ArrayList_praktiniai/AL_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
@@ -16,6 +16,9 @@ public class RememberTheseNumbers {
 
             numbers.add(luku);
         }
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
 
     }
 }
