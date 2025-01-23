6cd3b79d6cea91be84998a81bf240f9e69f3a444
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-23 17:24:54 +0400

    8.1uzd.08_11.NumberStatistics task compete. Kinda correct. Tests kinda passed, since comment out parts can ruint testing robot.

--- 8/8.1 uzduotis/08_11.NumberStatistics/src/main/java/MainProgram.java
+++ 8/8.1 uzduotis/08_11.NumberStatistics/src/main/java/MainProgram.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainProgram {
@@ -6,17 +7,58 @@ public class MainProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
+
+        //Statistics start = new Statistics();
+        //Can use above for new GENERAL SUM used for input part of exercise.
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+
+        //These bellow were commented out so not to intervene with part 2 of exercise.
+        //Where personal input is needed.
+
+        //statistics.addNumber(3);
+        //statistics.addNumber(5);
+        //statistics.addNumber(1);
+        //statistics.addNumber(2);
+        //System.out.println("Count: " + statistics.getCount());
+        //System.out.println("Sum: " + statistics.sum());
+        //System.out.println("Average: " + statistics.average());
+
+
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers,
-        // and lastly the one that tracks the sum of odd numbers!
+        //Sum of user input exercise starts here.
+        //ArrayList<Integer> list = new ArrayList<>() was not needed.
+        //As exercise asked to use only our Statistics Big Block.
+        System.out.println("Enter numbers:");
+        int input = 0;
+
+        //while (true) can write as well. Also, int input; without int input = 0;
+        while (input != -1) {
+            input = Integer.parseInt(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            statistics.addNumber(input);
+            if (input % 2 == 0) {
+                even.addNumber(input);
+            }
+            if (input % 2 != 0) {
+                odd.addNumber(input);
+            }
+
+        }
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + even.sum());
+        System.out.println("Sum of even numbers: " + odd.sum());
+
     }
 }
+
+
+// In order for the tests to work, the objects must be created in the
+// correct order in the main program. First the object that tracks the total
+// sum, secondly the object that tracks the sum of even numbers,
+// and lastly the one that tracks the sum of odd numbers!
+// Enter numbers: 4,2,5,2,-1. Sum = 13; Sum of even= 8; Sum of odd= 5
--- 8/8.1 uzduotis/08_11.NumberStatistics/src/main/java/Statistics.java
+++ 8/8.1 uzduotis/08_11.NumberStatistics/src/main/java/Statistics.java
@@ -2,16 +2,40 @@
 public class Statistics {
 
     private int count;
+    private int sum;
 
-    public Statistics(int numberCount) {
-        this.count = numberCount;
+    public Statistics() {
+        count = 0;
+        sum = 0;
         // initialize the variable numberCount here
     }
 
     public void addNumber(int number) {
+        count++;
+        sum = sum + number;
     }
 
     public int getCount() {
         return count;
+    }
+
+//    public int sum() {
+//        int number = 0;
+//        for (int i = 0; i < count; i++) {
+//            number = count;
+//            sum = sum + number;
+//
+//        }
+//        return sum;
+//    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        double average = (1.0 * sum) / count;
+        return average;
+        //Or just 1 line: return (1.0 * sum) / count;
     }
 }
